b330add093e57a050d893c24daf333b0f2c6b556
M A Manazir Ahsan <manazir.bd@gmail.com>
2015-12-31 18:30:45 +0600

    50145_admin_book_edit

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -21,9 +21,18 @@ class Admin::BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find params[:id]
   end
 
   def update
+    @book = Book.find params[:id]
+    if @book.update_attributes book_params
+      flash[:success] = t :books_edit_success_message
+      redirect_to [:admin, @book]
+    else
+      flash[:danger] = t :books_edit_failure_message
+      render :edit
+    end
   end
 
   def destroy
